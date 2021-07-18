f4ac16d643e8d5817162b1e06db7be4012c43172
Nathalia Tescarollo <nathtescarollo@gmail.com>
2021-07-18 17:51:47 -0300

    instalação e config inicial de dependências

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
+const bcryptjs = require('bcryptjs');
+
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -15,6 +19,14 @@ const app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+app.use(session({ 
+  secret: 'Aula de Session e Cookies',
+  resave: false,
+  saveUninitialized: true
+}));
+
+app.use(cookieParser());
 
 app.use(logger('dev'));
 app.use(express.json());
